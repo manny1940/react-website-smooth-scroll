2751a7aab633c0f950d0c98661d34ca982fc3459
manny1940 <madhureddy2019cse@gmail.com>
2021-06-21 08:23:57 +0530

    updated

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {FaBars} from 'react-icons/fa'
+import { 
+    Nav, 
+    NavbarContainer, 
+    NavLogo, 
+    MobileIcon, 
+    NavMenu, 
+    NavItem, 
+    NavLinks,
+    NavBtn,
+    NavBtnLink
+} from './NavbarElements'
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'>SeeKhu</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='about'>About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='explore'>Explore</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='services'>Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='signup'>Sign Up</NavLinks>
+                        </NavItem>
+                        <NavBtn>
+                            <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+                        </NavBtn>
+                    </NavMenu>
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
